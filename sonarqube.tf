6ac5764d3b2bc5e44d06abdf26ca8415b1e808b1
zeyangli <610556220@qq.com>
2022-05-21 16:44:53 +0800

    add sonarqube

---
+++ sonarqube.tf
@@ -0,0 +1,47 @@
+locals {
+  volumes = [
+    {
+      container_path = "/data/devops4/sonarqube/sonarqube_conf"
+      host_path      = "/opt/sonarqube/conf"
+    },
+    {
+      container_path = "/data/devops4/sonarqube/sonarqube_extensions"
+      host_path      = "/opt/sonarqube/extension"
+    },
+    {
+      container_path = "/data/devops4/sonarqube/sonarqube_logs"
+      host_path      = "/opt/sonarqube/logs"
+    },
+    {
+      container_path = "/data/devops4/sonarqube/sonarqube_data"
+      host_path      = "/opt/sonarqube/data"
+    }
+  ]
+}
+
+
+resource "docker_image" "sonar" {
+  name         = "sonarqube:8.9.8-community"
+  keep_locally = true //销毁时不删除本地镜像
+}
+
+resource "docker_container" "sonarqube" {
+  image = docker_image.sonar.name
+  name  = "sonarqube"
+  ports {
+    internal = 9000
+    external = 9000
+  }
+
+  dynamic "volumes" {
+    for_each = local.volumes
+    content {
+      container_path = volumes.value["container_path"]
+      host_path      = volumes.value["host_path"]
+    }
+  }
+  volumes {
+    container_path = "/var/jenkins_home"
+    host_path      = "/data/devops4/jenkins_home"
+  }
+}
